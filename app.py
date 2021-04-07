d34a5958cdc951a9583b141fae7a5d24f6e94a23
LazaroMPerez <lazpz61@gmail.com>
2021-04-07 00:12:26 -0400

    added workout endpoints

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+
+
 # from flask_heroku import Heroku
 # from flask_cors import CORS
 import os
@@ -17,8 +19,8 @@ ma = Marshmallow(app)
 # CORS(app)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
-    user = db.Column(db.String, unique=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, unique=False, nullable=False)
 
     def __init__(self, username, password):
@@ -27,24 +29,27 @@ class User(db.Model):
 
 class UserSchema(ma.Schema):
     class Meta:
-        fields = ("id", "username")
+        fields =("id", "username")
 
 user_schema = UserSchema()
 multiple_user_schema = UserSchema(many=True)
 
 
 class Workout(db.Model):
-    id= db.Column(db.Integer, primary_key= True)
+    id= db.Column(db.Integer, primary_key=True)
+    exercise = db.Column(db.String, nullable=False)
     muscle_group = db.Column(db.String, nullable=False)
     equiptment = db.Column(db.String, nullable=False)
+
     
-    def __init__(self, muscle_group, equiptment):
+    def __init__(self, exercise, muscle_group, equiptment):
+        self.exercise = exercise
         self.muscle_group = muscle_group
         self.equiptment = equiptment
 
 class WorkoutSchema(ma.Schema):
     class Meta:
-        fields = ("id", "muscle_group", "equiptment")
+        fields = ("id", "exercise", "muscle_group", "equiptment")
 
 workout_schema = WorkoutSchema()
 multiple_workout_schema = WorkoutSchema(many=True)
@@ -54,19 +59,51 @@ multiple_workout_schema = WorkoutSchema(many=True)
 @app.route("/user/add", methods=["POST"])
 def add_user():
     if request.content_type != "application/json":
-        return jsonify("Error: Data must be sent as JSON.")
+        return "Error: Data must be sent as JSON."
 
     post_data = request.get_json()
     username = post_data.get("username")
     password = post_data.get("password")
 
-# possible duplication error code to go here
+    possible_duplicate = db.session.query(User).filter(User.username == username).first()
+    if possible_duplicate is not None:
+        return jsonify("Error: choose another username")
+
 
     record = User(username, password)
     db.session.add(record)
     db.session.commit()
 
     return jsonify("User Added")
+
+@app.route("/user/get/", methods=["GET"])
+def get_all_users():
+    all_users = db.session.query(User).all()
+    return jsonify(multiple_user_schema.dump(all_users))
+
+# Workout Endpoints 
+@app.route("/workout/add", methods=["POST"])
+def add_workout():
+    if request.content_type != "application/json":
+        return "Error: Data must be sent as JSON."
+    
+    post_data = request.get_json()
+    exercise = post_data.get("exercise")
+    muscle_group = post_data.get("muscle_group")
+    equiptment = post_data.get("equiptment")
+
+    record = Workout(exercise, muscle_group, equiptment)
+    db.session.add(record)
+    db.session.commit()
+
+    return jsonify("Workout Added Successfully")
+
+# @app.route("/workout/add/mulitpl" , methods=["POST"])
+# def add_mulitpl_workouts():
+#     if request.content_type != "application/json":
+#         return jsonify("Error: Data must be sent as JSON.")
+    
+    
 
 
 
